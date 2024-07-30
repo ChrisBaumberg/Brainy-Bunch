3c956d90029899b3230e1a618ea01f78445c3cb6
ChrisBaumberg <das_ripchen@web.de>
2024-07-30 14:09:30 +0200

    Update 3007202401

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,27 @@
+require ("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const {v4: uuidv4}=require("uuid")
+const connectString = process.env.MONGO_DB_CLIENT;
+
+app.use(async(req, res, next)=>{
+  try{
+    await mongoose.connect(connectString);
+    console.log("Running connection");
+    next();
+  }
+  catch(e){
+    console.log("Running error")
+  }
+})
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
